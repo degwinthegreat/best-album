085c821b432d0c0b23f2af192138514f2640840c
degwinthegreat <the.colledge.dropout@gmail.com>
2019-12-30 14:08:04 +0900

    細身のレイアウトに変更

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -74,6 +74,6 @@ class AlbumsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def album_params
-      params.require(:album).permit(:rank, :url, :title, :artist_name, :posted_by)
+      params.require(:album).permit(:rank, :url, :title, :artist_name, :description, :posted_by)
     end
 end
---
+++ db/migrate/20191230044704_add_column_description_to_albums.rb
@@ -0,0 +1,5 @@
+class AddColumnDescriptionToAlbums < ActiveRecord::Migration[6.0]
+  def change
+    add_column :albums, :description, :text
+  end
+end
