b85cc66109fe3b2cefea83bb9f2386a963c14904
degwinthegreat <the.colledge.dropout@gmail.com>
2020-12-29 15:13:57 +0900

    ページネーションの数と投稿者数分にした

--- app/controllers/y2020_controller.rb
+++ app/controllers/y2020_controller.rb
@@ -2,15 +2,17 @@
 
 class Y2020Controller < ApplicationController
   def index
+    @posters = Album.y2020.distinct(:posted_by).pluck(:posted_by)
     @albums =
       if params[:posted_by]
         Album.y2020.where(posted_by: params[:posted_by])
              .order(rank: :desc).order(:created_at)
              .page(params[:page]).per(10)
       else
-        Album.y2020.order(rank: :desc).order(:created_at).page(params[:page]).per(7)
+        Album.y2020
+             .order(rank: :desc).order(:created_at)
+             .page(params[:page]).per(@posters.size)
       end
-    @posters = Album.y2020.distinct(:posted_by).pluck(:posted_by)
   end
 
   private
